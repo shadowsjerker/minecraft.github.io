14da221a2403435c6f559eae3a8d9dbf68088019
ShadowsJerker <shadowsjerker@gmail.com>
2021-08-14 23:11:44 +0430

    Changed to test for BLAZOR

---
+++ BlazorMovies/BlazorMovies/Shared/Entities/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMovies.Shared.Entities
+{
+    public class Movie
+    {
+        public string title { get; set; }
+        public DateTime releaseDate { get; set; }
+    }
+}
